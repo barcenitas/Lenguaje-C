aba1f04492763f2cf13973db64d9ca706a762c26
barcenitas <erick3arcenas7@gmail.com>
2018-03-13 00:14:35 -0600

    Metodo_Factores_Cuadraticos_parte_3

---
+++ prototiposlin.h
@@ -0,0 +1,9 @@
+#ifndef PROTOTIPOSLIN_H_INCLUDED
+#define PROTOTIPOSLIN_H_INCLUDED
+
+void ajustesdis();
+void ecsegundogrado(double A, double B, double C);
+double pregunta();
+double metodos();
+
+#endif // PROTOTIPOSLIN_H_INCLUDED
