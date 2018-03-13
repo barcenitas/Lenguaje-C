3fdea47ea3cbca03c5391cbc861e6586f0add600
barcenitas <erick3arcenas7@gmail.com>
2018-03-13 00:12:49 -0600

    Metodo_Factores_Cuadraticos_parte_1

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * Proyecto de Ingeniería 2: Calculadora de factores cuadráticos
+ *
+ * Integrantes:
+ *
+ * Alcántara Sánchez Katia Ixchel
+ * Bárcenas Martínez Erick Iván
+ * Fernández Monroy Patricia
+ * Steiger Avila Marc Alec
+ *
+ * Fecha de entrega: martes 13 de marzo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h >
+#include <windows.h>
+#include "prototiposlin.h"
+
+int main()
+{
+    ajustesdis();
+    pregunta();
+    metodos();
+    return 0;
+}
